4a988c1bf1a3353683ba20ffb064432cb9f88b12
Scott Davis <scottdavisxx@gmail.com>
2019-05-02 20:38:29 -0700

    new container with my stuff

--- vinson/scott.js
+++ vinson/scott.js
@@ -1,1 +1,8 @@
-console.log("Merge me!");
+const scottainer = document.createElement('div');
+document.body.appendChild(scottainer);
+scottainer.style['background-color'] = 'red';
+scottainer.style.borderBottom = 'black';
+scottainer.innerText = 'Scott';
+
+
+
